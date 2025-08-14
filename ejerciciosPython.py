5d1c8e13f8e02523915fc92f76b50d29f8dd0063
Santiago-Salazar122 <santiago.salzar@correo.tdea.edu.co>
2025-08-13 21:25:30 -0500

    actualizaciion

---
+++ ejerciciosPython.py
@@ -0,0 +1,9 @@
+temperatura = [22.5, 24.0, 23.8, 21.5, 20.0, 19.5, 22.0]
+print(temperatura[4])
+
+notas = [
+    [3.5, 4.0],
+    [4.2, 3.8],
+    [2.9, 3.0]
+]
+print("Segunda nota del tercer estudiante", notas[2][1])
---
+++ primer.py
@@ -0,0 +1,2 @@
+temperatura = [22.5, 24.0, 23.8, 21.5, 20.0, 19.5, 22.0]
+print(temperatura[4])
